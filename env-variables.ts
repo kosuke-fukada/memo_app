2793fa47c8a0209189754dd5d941240e3cf3472a
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2025-03-07 22:21:00 +0900

    prepare auth

--- env-variables.ts
+++ env-variables.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod'
 
 const envVariables = z.object({
-  DATABASE_URL: z.string().url()
+  DATABASE_URL: z.string().url(),
+  AUTH_GOOGLE_ID: z.string(),
+  AUTH_GOOGLE_SECRET: z.string(),
+  NEXTAUTH_URL: z.string().url(),
+  NEXTAUTH_SECRET: z.string()
 })
 
 envVariables.parse(process.env)
