2995e63cf0c887f126e48f94efac648c973745f3
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2025-03-11 22:02:32 +0900

    replace prisma to drizzle

--- auth.ts
+++ auth.ts
@@ -1,9 +1,12 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import { prisma } from 'lib/prisma'
+import { accounts, db, users } from '@/drizzle/schema'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts
+  }),
   providers: [Google]
 })
